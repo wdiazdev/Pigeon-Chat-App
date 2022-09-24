a160e89a49e936aebc6349fff9aa69b3ce82f4b8
Wilfredo Diaz <wdiazdev@gmail.com>
2022-09-24 13:58:58 -0400

    Search bar added

--- src/components/sidebar/Navbar.js
+++ src/components/sidebar/Navbar.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <span className='navbar--logo'>logo</span>
+            <span className='navbar--logo'>Pigeon</span>
             <div className='navbar--user'>
-                <img src='https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='User Image'></img>
-                <span>John</span>
+                <img src='https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='User Info' />
+                <span>John Smith</span>
                 <button className='navbar--button'>Logout</button>
             </div>
         </div>
--- src/components/sidebar/Search.js
+++ src/components/sidebar/Search.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div>Its working</div>
+        <div className='search--bar'>
+            <div className='search--form'>
+                <input type='text' placeholder='Find user' />
+            </div>
+            <div className="sidebar--user--info">
+                <img src='https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='User Info' />
+                <div className="user--info">
+                    <span>John Smith</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
