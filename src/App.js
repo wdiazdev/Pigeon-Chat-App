4747feea9ede2b8f4887c8ba032577b33ee06917
Wilfredo Diaz <wdiazdev@gmail.com>
2022-10-01 18:03:13 -0400

    Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import Home from "./pages/Home"
 import Login from "./pages/Login";
@@ -19,7 +19,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/">
           <Route index element={
@@ -30,7 +30,7 @@ function App() {
           <Route path="Register" element={<Register />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter >
   );
 }
 
